326959e63bda203e142efdfb86e638b47d13faf2
murtaz25raina <42265864+murtaz25raina@users.noreply.github.com>
2018-09-14 14:43:07 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,56 +1,92 @@
-class  ArrayListClass
+class ArrayList1
 {
 	private int a[];
-	private int indexes;
+	private int index;
 	private int size;
-	ArrayListClass(int size)
+	private int Size;
+	ArrayList1(int size)
 	{
 		a=new int[size];
-		indexes=-1;
 		this.size=size;
+		index=-1;
+		Size=size;
 	}
-	public void add(int num)
+	public void add(int data)
 	{
-		if(indexes==this.size-1)
+		if(index==size-1)
 		{
 			int temp[]=a;
-			this.size=this.size+(this.size/2);
+			this.size=this.size*2;
 			a=new int[this.size];
-			for(int i=0;i<=indexes;i++)
-			{
+			for(int i=0;i<=index;i++)
 				a[i]=temp[i];
-			}
-			indexes++;
-			a[indexes]=num;
+			index++;
+			a[index]=data;
 		}
-		else
-		{
-			indexes++;
-			a[indexes]=num;
-		}
+		else{
+			index++;
+		  a[index]=data;}
 	}
 	public void traverse()
 	{
-		for(int i=0;i<=indexes;i++)
-			System.out.print(a[i]+ " ");
-	}
-	public int get(int n)
+		if(index==-1)
+		{
+			System.out.print("empty");
+		}
+		else{
+		for(int i=0;i<=index;i++)
+			System.out.print(a[i]+" ");
+	}}
+	public void remov(int pos)
 	{
-		if(indexes>n)
-		return a[n];
+		for(int i=pos;i<=index;i++)
+		{
+			a[i]=a[i+1];
+		}
+		a[index]=0;
+		index--;
+	}
+	public void remall()
+	{
+		
+		a=new int[Size];
+		index=-1;
+	}
+	public int get(int pos)
+	{
+		if(pos<=index)
+		{
+			return a[pos];
+		}
 		else
-		return -1;
+			return -1;
 	}
 }
 public class ArrayList {
 
 	public static void main(String[] args) {
-        ArrayListClass l1=new ArrayListClass(5);		
-         for(int i=1;i<=5;i++)
-        	 l1.add(i*10);
-         l1.add(100);
-         l1.traverse();
-		 int n=3;
-		 System.out.println(l1.get(3));
+     
+		ArrayList1 l1= new ArrayList1(5);
+		for(int i=1;i<=5;i++)
+			l1.add(i*10);
+		l1.add(5);
+		l1.traverse();
+		System.out.println();
+		l1.remov(3);
+		l1.traverse();
+		System.out.println();
+		l1.add(7);
+		l1.traverse();
+		l1.remall();
+		System.out.println();
+		l1.traverse();
+		System.out.println();
+		l1.add(7);
+		l1.traverse();
+		System.out.println();
+		l1.add(8);
+		l1.traverse();System.out.println();
+		System.out.println(l1.get(1));
 	}
+
 }
